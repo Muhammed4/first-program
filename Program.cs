b87444689679aeb3d59b9142577f346ddbb336dc
Muhammed4 <namawulo2000@gmail.com>
2022-02-08 17:17:29 +0300

    no commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, toplam;
+            Console.WriteLine("Calculate the sum of two numbers:");
+            Console.Write("Input number1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input number2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            toplam = num1 + num2;
+            Console.Write("SONUÇ:" + toplam);
+
+            Console.ReadKey();
+        }
+    }
+}
